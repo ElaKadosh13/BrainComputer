c706c1f5b887b9ddb8e48de4cfcc9b9135978cdd
Ela Kadosh <ela.kadosh13@gmail.com>
2019-11-30 17:11:41 +0200

    Added req & setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-# BrainComputer
+from setuptools import setup, find_packages
+setup(
+    name = 'braincomputer',
+    version = '0.1.0',
+    author = 'Ela Kadosh',
+    description = 'hardware that can read minds',
+    packages = find_packages(),
+    install_requires = ['click', 'flask'],
+    tests_require = ['pytest', 'pytest-cov'],
+)
