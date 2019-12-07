4f56c7dbf3740b0749da0e152955164c4c81e6a3
Ela Kadosh <ela.kadosh13@gmail.com>
2019-12-07 12:43:12 +0200

    Added click Cli

--- braincomputer/client.py
+++ braincomputer/client.py
@@ -1,34 +1,11 @@
-import socket   
-import struct
-import time
-import calendar
+import datetime
 
-from braincomputer.cli import CommandLineInterface
+from braincomputer.thought import Thought
+from braincomputer.utils import Connection
 
-cli = CommandLineInterface()
-
-def pack_message_protocol(user_id, thought):
-    struct_obj = struct.Struct('LLI') 
-    time_stamp = calendar.timegm(time.gmtime())
-    return struct_obj.pack(user_id, time_stamp, len(thought))   
-
-@cli.command
-def upload_thought(address, user, thought):
-    connection = socket.socket()
+def upload_thought(address, user_id, thought):
     tuple_address = address.split(":")
-    arg_address = (tuple_address[0], int(tuple_address[1]))
-    connection.connect(arg_address)
-    message_protocol = pack_message_protocol(int(user), thought)
-    message = message_protocol + thought.encode()
-    connection.sendall(message)
-    print("done")
-    connection.close()
+    with Connection.connect(tuple_address[0], int(tuple_address[1])) as connection:
+        serialized_thought = Thought(user_id, datetime.datetime.now(), thought).serialize()
+        connection.send(serialized_thought)
 
-def main(argv):
-    cli.main()
-
-
-if __name__ == '__main__':
-    import sys
-    cli.main()
-    sys.exit()
