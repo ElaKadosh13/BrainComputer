5d65dab2a30944cbe7207072589faca23297df9a
Ela Kadosh <ela.kadosh13@gmail.com>
2019-12-07 18:17:43 +0200

    fixed testsing

--- braincomputer/__init__.py
+++ braincomputer/__init__.py
@@ -1,5 +1,5 @@
-# from .client import upload_thought
-# from .server import run_server
-# from .web import run_webserver
+from .client import upload_thought
+from .server import run_server
+from .web import run_webserver
 
 version = "0.1.0"
