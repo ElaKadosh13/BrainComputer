13f832fa6d9c02585b08f626873ca918d7483882
Ela Kadosh <ela.kadosh13@gmail.com>
2020-04-12 19:07:21 +0300

    sphinx & read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'BrainComputer'
-copyright = '2020, Ela Kadosh'
-author = 'Ela Kadosh'
+copyright = '2020, ElaKadosh'
+author = 'ElaKadosh'
+
+# The full version, including alpha/beta/rc tags
+release = '1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
